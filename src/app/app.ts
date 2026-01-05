4af40f4287a7abad5ca1ca359346b496c3dac58b
Żenia <zenasamarash@gmail.com>
2026-01-06 00:18:04 +0100

    Fix info for me.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {RouterOutlet} from '@angular/router';
 import {SocialMedia} from './social-media/social-media';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, SocialMedia],
+  imports: [RouterOutlet, SocialMedia],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
